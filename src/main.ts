d5d36208b397bcb9b1299de880b0df16821b8abf
joshuajewell <enquiries@joshuajewell.dev>
2024-06-23 23:39:48 +0100

    Bring sample platformer from Excalibur

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import * as ex from 'excalibur';
+import { loader } from './resources';
+import { Level } from './level';
+
+const engine = new ex.Engine({
+    backgroundColor: ex.Color.fromHex('#efefef'),
+    width: 600,
+    height: 400,
+    fixedUpdateFps: 60,
+    // Turn off anti-aliasing for pixel art graphics
+    antialiasing: false
+});
+
+// Set global gravity in pixels/sec^2
+ex.Physics.acc = new ex.Vector(0, 800);
+
+// Setup first level as a custom scene
+const level = new Level();
+engine.add('level', level);
+engine.goToScene('level');
+
+// Game events to handle
+engine.on('hidden', () => {
+    console.log('pause');
+    engine.stop();
+});
+engine.on('visible', () => {
+    console.log('start');
+    engine.start();
+});
+
+// Start the engine
+engine.start(loader).then(() => {
+    console.log('game start');
+});
+
+// For test hook
+(window as any).engine = engine;
+(window as any).level = level;
