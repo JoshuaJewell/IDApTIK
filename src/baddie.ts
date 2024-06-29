aaadbf9783b9c86a687f59b3b69106353129c92a
joshuajewell <enquiries@joshuajewell.dev>
2024-06-29 16:32:02 +0100

    Developed jump mechanic, with errors

--- src/baddie.ts
+++ src/baddie.ts
@@ -49,7 +49,7 @@ export class Baddie extends ex.Actor {
     }
 
     onPostCollision(evt: ex.PostCollisionEvent) {
-        if (evt.other instanceof Bot && (evt.side === ex.Side.Top || ((bot.killing = true)))) {
+        if (evt.other instanceof Bot && (evt.side === ex.Side.Top)) {
             Resources.gotEm.play(.1);
             // Clear patrolling
             this.actions.clearActions();
